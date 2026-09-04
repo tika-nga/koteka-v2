9ad38fead7063e5bfe101fd2315250e7c69d1f4d
tika-nga <junior.sonde@gmail.com>
2026-09-04 13:20:37 +0200

    Update navigation_view_model.dart

--- lib/view_models/navigation_view_model.dart
+++ lib/view_models/navigation_view_model.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_marketplace_template/screens/home_screen.dart';
 import 'package:flutter_marketplace_template/screens/map_screen.dart';
+import 'package:flutter_marketplace_template/screens/post_ad_screen.dart';
+import 'package:flutter_marketplace_template/screens/chats_list_screen.dart';
 import 'package:flutter_marketplace_template/screens/profile_screen.dart';
 
 class NavigationViewModel extends ChangeNotifier {
   NavigationViewModel({this.selectedIndex = 0});
 
   int selectedIndex;
-  Widget currentScreen = HomeScreen();
+
+  Widget currentScreen = const HomeScreen();
 
   final List<Widget> screens = const [
-    HomeScreen(),
-    MapScreen(),
-    ProfileScreen(),
+    HomeScreen(),       // 0 Accueil
+    MapScreen(),        // 1 Rechercher
+    PostAdScreen(),     // 2 Déposer
+    ChatsListScreen(),  // 3 Messages
+    ProfileScreen(),    // 4 Profil
   ];
 
   void onDestinationSelected(int index) {
