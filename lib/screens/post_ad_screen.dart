30f6e65e645d85a39c0889dde83527345ee6671a
tika-nga <junior.sonde@gmail.com>
2026-09-10 23:40:57 +0200

    Update post_ad_screen.dart

--- lib/screens/post_ad_screen.dart
+++ lib/screens/post_ad_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 class PostAdScreen extends StatefulWidget {
   const PostAdScreen({super.key});
 
