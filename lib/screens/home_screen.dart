308a0818e2805ac9dc7683631992c97d6003822e
tika-nga <junior.sonde@gmail.com>
2026-09-16 11:00:02 +0200

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -499,6 +499,7 @@ FutureBuilder<List<Map<String, dynamic>>>(
     );
   },
 ),
+                        
 }
                         
   /// Builds the action bar with reset filters button and sorting menu
