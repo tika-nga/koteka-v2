f727ed16e53d0f75a1038706d3a7cde17e45f01d
tika-nga <junior.sonde@gmail.com>
2026-09-16 13:53:39 +0200

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_marketplace_template/models/place.dart';
+import 'package:flutter_marketplace_template/screens/place_screen.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_marketplace_template/functions.dart';
 import 'package:flutter_marketplace_template/l10n/app_localizations.dart';
