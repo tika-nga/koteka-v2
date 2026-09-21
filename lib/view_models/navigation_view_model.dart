8bc73dc088216a2621be61d9028f4d2f9421fc82
tika-nga <junior.sonde@gmail.com>
2026-09-22 00:46:11 +0200

    Update navigation_view_model.dart

--- lib/view_models/navigation_view_model.dart
+++ lib/view_models/navigation_view_model.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_marketplace_template/screens/home_screen.dart';
-import 'package:flutter_marketplace_template/screens/map_screen.dart';
 import 'package:flutter_marketplace_template/screens/post_ad_screen.dart';
 import 'package:flutter_marketplace_template/screens/chats_list_screen.dart';
 import 'package:flutter_marketplace_template/screens/profile_screen.dart';
 
 class NavigationViewModel extends ChangeNotifier {
-  NavigationViewModel({this.selectedIndex = 0});
+  NavigationViewModel({
+    this.selectedIndex = 0,
+  });
 
   int selectedIndex;
 
@@ -15,13 +16,16 @@ class NavigationViewModel extends ChangeNotifier {
 
   final List<Widget> screens = const [
     HomeScreen(),       // 0 Accueil
-    MapScreen(),        // 1 Rechercher
-    PostAdScreen(),     // 2 Déposer
-    ChatsListScreen(),  // 3 Messages
-    ProfileScreen(),    // 4 Profil
+    PostAdScreen(),     // 1 Déposer
+    ChatsListScreen(),  // 2 Messages
+    ProfileScreen(),    // 3 Profil
   ];
 
   void onDestinationSelected(int index) {
+    if (index < 0 || index >= screens.length) {
+      return;
+    }
+
     selectedIndex = index;
     currentScreen = screens[index];
     notifyListeners();
