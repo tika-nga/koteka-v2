230b2891cbbb08f714f04d489e479ff3adb78c49
tika-nga <junior.sonde@gmail.com>
2026-09-21 23:43:25 +0200

    Update theme_view_model.dart

--- lib/view_models/theme_view_model.dart
+++ lib/view_models/theme_view_model.dart
@@ -1,29 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_marketplace_template/services/theme_service.dart';
-import 'package:flutter_marketplace_template/models/profile_preferences_enums.dart';
 
 class ThemeViewModel extends ChangeNotifier {
-  final IThemeService _themeService;
+  ThemeViewModel([Object? unused]);
 
-  AppThemeMode _mode = AppThemeMode.automatic;
-
-  AppThemeMode get mode => _mode;
-
-  ThemeViewModel(this._themeService) {
-    _load();
-  }
-
-  Future<void> _load() async {
-    final flutterMode = await _themeService.getThemeMode();
-    _mode = AppThemeModeExt.fromFlutter(flutterMode);
-    notifyListeners();
-  }
-
-  Future<void> setMode(AppThemeMode m) async {
-    _mode = m;
-    await _themeService.setThemeMode(m.toFlutter());
-    notifyListeners();
-  }
-
-  ThemeMode get flutterMode => _mode.toFlutter();
+  ThemeMode get flutterMode => ThemeMode.light;
 }
