78018ee98d5ae1fbb6c9c6d9ca9a2b050014b533
tika-nga <junior.sonde@gmail.com>
2026-09-25 22:12:43 +0200

    Update navigation_screen.dart

--- lib/screens/main/navigation_screen.dart
+++ lib/screens/main/navigation_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:flutter_marketplace_template/view_models/navigation_view_model.dart';
+import 'package:flutter_marketplace_template/view_models/chats_list_view_model.dart';
 
 class NavigationScreen extends StatefulWidget {
   const NavigationScreen({super.key});
